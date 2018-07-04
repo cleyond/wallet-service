792361097c11c4d779f9f14550499ecf1b8eae37
Matheus Rezende <matheus.rezende10@gmail.com>
2018-07-04 20:41:43 +0700

    Implemented validate btc address endpoint #6 (#11)

--- src/services/btc.services.js
+++ src/services/btc.services.js
@@ -1,8 +1,8 @@
 /*
  * @Author: Matheus Rezende
  * @Date: 2018-07-04 19:54:44
- * @Last Modified by:   @matheusrezende
- * @Last Modified time: 2018-07-04 19:54:44
+ * @Last Modified by: @matheusrezende
+ * @Last Modified time: 2018-07-04 20:30:51
  */
 import bitcore from 'bitcore-lib'
 
@@ -15,4 +15,17 @@ import bitcore from 'bitcore-lib'
 export const createBitcoreAddress = () => {
   const privateKey = new bitcore.PrivateKey();
   return privateKey.toAddress();
+}
+
+
+/**
+ * @function validateAddress
+ *
+ * @returns {Bool} return if the address is valids
+ */
+export const validateBitcoreAddress = (address) => {
+  console.log('-------------------')
+  console.log(address)
+  console.log('-------------------')
+  return bitcore.Address.isValid(address)
 }
