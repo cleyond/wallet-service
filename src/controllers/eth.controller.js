3c832a7fe01cc9ed150d8df1ea056888445434af
Matheus Rezende <matheus.rezende10@gmail.com>
2018-07-05 15:41:53 +0700

    Implemented transaction information endpoint #4

--- src/controllers/eth.controller.js
+++ src/controllers/eth.controller.js
@@ -2,7 +2,7 @@
  * @Author: Matheus Rezende
  * @Date: 2018-07-04 19:55:35
  * @Last Modified by: @matheusrezende
- * @Last Modified time: 2018-07-05 14:34:04
+ * @Last Modified time: 2018-07-05 14:58:31
  */
 import HTTPStatus from 'http-status';
 import Joi from 'joi'
@@ -14,6 +14,11 @@ export const validation = {
       address: Joi.string().required(),
     },
   },
+  getTxInfo: {
+    params: {
+      txHash: Joi.string().required(),
+    },
+  },
 }
 
 
@@ -21,6 +26,16 @@ export const validateEthAddress = (req, res, next) => {
   try {
     const responseBody = {isValid: ETHServices.validateEthAddress(req.body.address)}
     return res.status(HTTPStatus.OK).json(responseBody)
+  } catch (err) {
+    err.status = HTTPStatus.BAD_REQUEST;
+    return next(err);
+  }
+}
+
+export const getTxInfo = async (req, res, next) => {
+  try {
+    const result = await ETHServices.getTxInformation(req.params.txHash)
+    return res.status(HTTPStatus.OK).json(result)
   } catch (err) {
     err.status = HTTPStatus.BAD_REQUEST;
     return next(err);
--- src/services/eth.services.js
+++ src/services/eth.services.js
@@ -2,7 +2,7 @@
  * @Author: Matheus Rezende
  * @Date: 2018-07-05 14:29:17
  * @Last Modified by: @matheusrezende
- * @Last Modified time: 2018-07-05 14:30:25
+ * @Last Modified time: 2018-07-05 15:39:50
  */
 
 import web3 from '../config/web3'
@@ -10,7 +10,18 @@ import web3 from '../config/web3'
 
 /**
  * @function validateAddress
- *
+ * @param {String} address
  * @returns {Bool} return if the address is valid or not
  */
 export const validateEthAddress = (address) => web3.utils.isAddress(address)
+
+/**
+ * @function getTxInformation
+ * @param {String} txHash
+ * @returns {Bool} return if the address is valid or not
+ */
+export const getTxInformation = async (txHash) => {
+  const transaction = await web3.eth.getTransaction(txHash)
+  const receipt = await web3.eth.getTransactionReceipt(txHash)
+  return {transaction, receipt}
+}
